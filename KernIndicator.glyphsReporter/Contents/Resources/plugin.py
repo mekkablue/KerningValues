59ef303f04bf749575fccf298bc8d8b892bc7dbf
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-11-14 17:38:39 +0100

    Initial Release

---
+++ KernIndicator.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,90 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Reporter Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Reporter
+#
+#
+###########################################################################################################
+
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class KernIndicator(ReporterPlugin):
+
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': u'Kerning Values',
+			'de': u'Kerning-Werte',
+			'es': u'valores de Kerning',
+			'fr': u'valeurs de crénage',
+		})
+		
+	def foreground(self, layer):
+		positiveColor = NSColor.systemOrangeColor()
+		negativeColor = NSColor.systemBlueColor()
+		
+		# go through tab content
+		glyph = layer.glyph()
+		font = layer.font()
+		if font: # sometimes font is empty, don't know why
+			tabView = font.currentTab.graphicView()
+			layerCount = tabView.cachedLayerCount()
+			lineOfLayers = []
+			lineOfOffsets = []
+			maxHeight = 600.0 # where to draw the kerning value
+			
+			# collect layers in the same line (to only draw the marks we need)
+			for i in range(layerCount):
+				thisLayer = tabView.cachedGlyphAtIndex_(i)
+			
+				# collect layers and their offsets except newlines
+				if type(thisLayer) != GSControlLayer:
+					lineOfLayers.append( thisLayer )
+					lineOfOffsets.append( tabView.cachedPositionAtIndex_(i) )
+					layerBounds = thisLayer.bounds
+					maxHeight = max( maxHeight, layerBounds.origin.y+layerBounds.size.height+150 )
+			
+				# if we reach end of line or end of text, draw with collected layers:
+				if type(thisLayer) == GSControlLayer or i==layerCount-1:
+					previousLayer = None
+					# step through all layers of the line:
+					for j, thisLayerInLine in enumerate(lineOfLayers):
+						if previousLayer and previousLayer.associatedFontMaster().id == thisLayerInLine.associatedFontMaster().id:
+							masterID = thisLayerInLine.associatedFontMaster().id
+							leftGlyph = previousLayer.glyph()
+							rightGlyph = thisLayerInLine.glyph()
+		
+							kerningValue = None
+							if leftGlyph and rightGlyph:
+								kerningValue = font.kerningForFontMasterID_firstGlyph_secondGlyph_( masterID, leftGlyph, rightGlyph )
+								if kerningValue > 10000: #notfound
+									kerningValue = None
+							
+							if not kerningValue is None:
+								activePosition = tabView.activePosition()
+								lastLayerInLinePosition = tabView.cachedPositionAtIndex_(i)
+								thisLayerInLinePosition = lineOfOffsets[j]
+								offset = subtractPoints(thisLayerInLinePosition, activePosition)
+								textPoint = NSPoint( offset.x/self.getScale() - 0.5*kerningValue, maxHeight )
+								if kerningValue < 0:
+									self.drawTextAtPoint( str(int(kerningValue)), textPoint, fontColor=negativeColor, align="bottomcenter" )
+								else:
+									self.drawTextAtPoint( str(int(kerningValue)), textPoint, fontColor=positiveColor, align="bottomcenter" )
+									
+						previousLayer = thisLayerInLine
+				
+					# reset layer collection and maxHeight
+					lineOfLayers = []
+					lineOfOffsets = []
+					maxHeight = 600.0
+		
+
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# KerningValues
-Glyphs.app plugin for showing the kerning values above the typed text in Edit view.
+# Kerning Values
+
+This is a plugin for the font editor Glyphs.app. It shows the kerning values above the typed text in Edit view, like this:
+
+![KerningIndicators](KerningIndicators.png)
+
+### Installation
+
+1. One-click install *Kerning Values* from *Window > Plugin Manager.*
+2. Restart Glyphs.
+
+### Usage Instructions
+
+1. Open at least two glyphs in Edit View.
+2. Use *View > Show Kerning Values* (de: *Ansicht > Zeige Kerning-Werte*, es: *Ver > Mostrar valores de Kerning*, fr: *Affichage > Afficher valeurs de crénage*) to toggle the display of the kerning values.
+
+It will not display if you put the cursor *after* all text in Edit view.
+
+### Requirements
+
+The plug-in works in Glyphs 2.5.2 in High Sierra. I can only test it in current app and OS versions, and perhaps it works on earlier versions too.
+
+### License
+
+Copyright 2018 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
